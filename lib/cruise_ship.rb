1b2cc71599fca05eca962490969b70dbb951a5d4
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-29 17:50:51 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -15,10 +15,11 @@ def select_winner(passengers)
 end
  
 winner
-
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
 end
+
+
+# def happy_birthday(birthday_kids)
+#   birthday_kids.each do |kids_name, age|
+#     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+#   end
+# end
