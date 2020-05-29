db7dd956222fb2faefea5b95655c16c3b29cef06
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-29 17:59:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do|kids_name, age|
-  if 
-  puts "Happy Birthday #{kids_name}! You are now #{age} years old!
+  if age < 10
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else 
+    puts
 end
